7e094544eb3ff127bc27cdc65286bcb88480142c
wangyanglin <wangyanglin@szkct.cn>
2018-08-11 17:25:26 +0800

    测试版本A

--- app/src/main/java/com/gittest/yz/MainActivity.java
+++ app/src/main/java/com/gittest/yz/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("版本11111111111111111111");
     }
 }
